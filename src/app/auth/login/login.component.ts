6fb265df160e8fd0225d424c61b374f4f1cdbd75
Miguel Aguila <maguila.delgado@gmail.com>
2024-10-25 16:01:14 -0300

    pantalla de login + estilos

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,35 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule , ButtonModule],
+  imports: [CommonModule, FormsModule , ButtonModule, DividerModule, InputTextModule, PanelModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  usuario?:string
+  password?:string
+  
+  constructor(private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object 
+  ){    
+  }
+
+  ngOnInit(): void {
+    // Verifica si estamos en el entorno del navegador antes de acceder al document
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.addClass(document.body, 'body-background');      
+    }
+    
+  }
+
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private authenticated = signal(false)
+
   constructor() { }
+
+  login(username: string, password: string){
+    this.authenticated.set(true)
+  }
+
+  isAuthenticated() {
+    return this.authenticated()
+  }
+
+  public getPrecios(){
+
+  }
 }
